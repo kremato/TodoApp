2136cd4cb99384ef7c17168b2fb283a6e3ab526e
Jakub Krajčík <jakubkrajcik1@gmail.com>
2022-07-05 20:09:27 +0200

    refactor: date is now shown as dd/MM/yyyy

--- app/src/components/TodoList/AddNewTodoItem.tsx
+++ app/src/components/TodoList/AddNewTodoItem.tsx
@@ -117,7 +117,7 @@ export const AddNewTodoLItem = ({ catalogueId }: INewTodoItemProps) => {
                     <>
                       <DesktopDatePicker
                         label="Deadline"
-                        inputFormat="MM/dd/yyyy"
+                        inputFormat="dd/MM/yyyy"
                         value={date}
                         onChange={(newValue) => {
                           setDate(newValue);
